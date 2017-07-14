386b167c640d8d79d0ba91e835ac77e8ba9283f5
Bjorn Tipling <bjorn@imgur.com>
2017-07-13 22:58:06 -0700

    Listing recursive directory and file contents.

--- src/FileSystem/Directory.hs
+++ src/FileSystem/Directory.hs
@@ -1,8 +1,12 @@
 module FileSystem.Directory (getPathContents, listContents, validate) where
 
 import qualified System.Directory as Directory
+import qualified System.IO as IO
 import qualified System.IO.Error as Error
 import qualified Control.Exception as Exception
+import qualified Control.Monad as Monad
+import qualified Debug.Trace as Trace
+import qualified System.FilePath as FilePath
 
 validate :: String -> IO Bool
 validate path = Directory.doesDirectoryExist path
@@ -14,12 +18,31 @@ listContents path = do
         Left _        -> return Nothing
         Right entries -> return (Just (foldl (\acc entry -> acc ++ " \n -> " ++ entry) "" entries))
 
+fileContents :: String -> IO (Maybe String)
+fileContents path = do
+    c <- Exception.try $ IO.readFile path :: IO (Either Exception.IOException String)
+    case c of
+        Left _         -> return Nothing
+        Right contents -> return $ Just contents
+
 getPathContents :: String -> [String] -> IO (Maybe String)
 getPathContents rootPath pathList = let
-    pList = filter notEmpty pathList
-    in (case pList of
-         [] -> listContents rootPath
-         pList -> return $ Just "something")
+    validList = filter notEmpty pathList
+    in (case validList of
+         []        -> listContents rootPath
+         validList -> getPathContents' rootPath validList)
+
+getPathContents' :: String -> [String] -> IO (Maybe String)
+getPathContents' currentPath pathList = do
+    if null pathList then listContents currentPath
+    else do
+        let nextPath     = currentPath ++ [FilePath.pathSeparator] ++ (head pathList)
+        isValidDirectory <- Directory.doesDirectoryExist nextPath
+        if isValidDirectory then getPathContents' nextPath (tail pathList)
+        else do
+            isValidFile <- Directory.doesFileExist nextPath
+            if isValidFile then fileContents nextPath
+            else return Nothing
 
 notEmpty :: String -> Bool
 notEmpty s = not $ null s
